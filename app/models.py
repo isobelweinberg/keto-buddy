b6cabd60db239d2781481e5f2f2fcb76cf5cce6d
Isobel Weinberg <isiweinberg@gmail.com>
2025-07-09 23:25:21 +0100

    Password hash length

--- app/models.py
+++ app/models.py
@@ -75,7 +75,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     childsname = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
