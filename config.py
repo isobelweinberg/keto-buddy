882579db9eedbe67ccc5c3840843035a880f4807
Isobel Weinberg <isiweinberg@gmail.com>
2025-07-09 19:35:03 +0100

    Config

--- config.py
+++ config.py
@@ -7,6 +7,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     load_dotenv()
 
+    db_uri = os.environ.get('DATABASE_URL')
+
+    if db_uri and db_uri.startswith("postgres://"):
+        db_uri = db_uri.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = db_uri or 'sqlite:///' + os.path.join(basedir, 'keto.db')
+
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'keto.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
