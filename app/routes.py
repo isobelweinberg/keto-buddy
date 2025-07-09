cb5627d732f093b0e0379f5fd6f167705c185667
Isobel Weinberg <isiweinberg@gmail.com>
2025-07-09 11:52:18 +0100

    Minor changes

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from datetime import date, timedelta
 from collections import defaultdict
 
-from flask import Blueprint, render_template, request, redirect, session, url_for, flash
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from . import db
 from .models import Ingredient, Recipe, RecipeIngredient, Target, TargetBreakdown, User, PlannerEntry, LogEntry
